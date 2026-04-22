28fe93565ad9fa16e4f1cd087f024a5e984ba5eb
シゴレフ　エドワード <eduard@hsc.ac.jp>
2026-04-22 11:56:31 +0900

    Implement file creation and writing for animals and fruits
    Add functionality to create and write to animals.txt and fruits.txt files.

--- 課題.py
+++ 課題.py
@@ -1,16 +1,17 @@
 #1
+# animals.txtファイルを生成して、動物（3匹）を追加して
 with open("animals.txt","r") as animals_file:
     for animals in animals_file:
        print("名称：",animals.strip())
 
-#2
+#2 OK
 with open("fruits.txt","w") as fruits_file:
     fruits_file.write("apple","banana","orange")
 with open(fruits_file,"r") as fruits_file:
     for fruits in fruits_file:
         print("名称：",fruits.strip())
 
-#3
+#3 OK
 def write_list(todo):
     with open("task.txt","a") as task_file:
         task_file.write(todo+"\n")
