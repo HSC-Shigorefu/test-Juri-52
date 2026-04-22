b660e7a651c16a31944572e83079dc883c5f0cf5
Juri <5251016@st.hsc.ac.jp>
2026-04-22 11:52:19 +0900

    課題提出

---
+++ 課題.py
@@ -0,0 +1,23 @@
+#1
+with open("animals.txt","r") as animals_file:
+    for animals in animals_file:
+       print("名称：",animals.strip())
+
+#2
+with open("fruits.txt","w") as fruits_file:
+    fruits_file.write("apple","banana","orange")
+with open(fruits_file,"r") as fruits_file:
+    for fruits in fruits_file:
+        print("名称：",fruits.strip())
+
+#3
+def write_list(todo):
+    with open("task.txt","a") as task_file:
+        task_file.write(todo+"\n")
+def read_list():
+    with open("task.txt", "r") as task_file:
+        for task in task_file:
+            print("やること：", task.strip())
+
+write_list(input("ToDoリストに入力："))
+read_list()
